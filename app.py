3ab51b10e08aeba24491c187d297dfb2291d26e4
Renan Vital <renan_andrade_90@hotmail.com>
2025-02-27 19:34:46 -0300

    ::feat:embedding

---
+++ app.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import pandas as pd
+from recommender import model
+
+st.title('Recommender systems')
+st.markdown("Data scientist: Renan Vital")
+st.divider()
+purchased_games = st.multiselect(label='Purchase games:',
+                options=['The Elder Scroll Svskyrim','Fallout 4','Spore','Left 4 Dead 2']
+                )
+#
+df_feat = pd.read_parquet('https://swp-mvp-media.s3.us-east-1.amazonaws.com/data/02_processed/ABT_feats.parquet')
+st.dataframe(df_feat.head())
+# purchased_games_str = [re.sub('[^A-Za-z0-9]+', '', i) for i in purchased_games]
+# purchased_games_str = [i.lower() for i in purchased_games_str]
+# purchased_games_str = [i.replace(' ','') for i in purchased_games_str]
+# df_selected_purchased = df_feat[df_feat.FK_GAME_NAME.isin(purchased_games_str)]
+# df_content = df_selected_purchased.drop_duplicates(subset='FK_GAME_NAME').copy()
+# df_content['explicative_features'] =\
+#   df_content['Tags'].astype(str)\
+#   + '|' + df_content['Categories'].astype(str)\
+#   + '|' + df_content['Supported languages'].astype(str)\
+#   + '|' + df_content['Estimated owners'].astype(str)\
+#   + '|' + df_content['Price'].astype(str)
+# df_content['explicative_features'] = df_content['explicative_features'].astype(str).str.replace("'",'No description').str.replace("None",'No description')
+# explicative_features = ' '.join(df_content['explicative_features'].to_list())
+# st.markdown(explicative_features)
+# st.dataframe(df_feat[df_feat.FK_GAME_NAME.isin(purchased_games_str)])
+content = model.recommender(purchased_games, df_feat)
+st.markdown(content)
+
+if len(purchased_games)==0:
+    st.header("Welcome to the plaform! You don't have any game yet, so here are popular games:")
+    col_norec_a, col_norec_b, col_norec_c = st.columns(3)
+    with col_norec_a:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+    with col_norec_b:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+    with col_norec_c:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+else:
+    st.header('Hybrid recommendation - Based on your own games and players with similar interests:')
+    col_hybrid_a, col_hybrid_b, col_hybrid_c = st.columns(3)
+    with col_hybrid_a:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+    with col_hybrid_b:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+    with col_hybrid_c:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+    
+    st.header('Recommendations based on your games:')
+    col_content_a, col_content_b, col_content_c = st.columns(3)
+    with col_content_a:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+    with col_content_b:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+    with col_content_c:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+    
+    st.header('Recommendations based on similar users:')
+    col_colab_a, col_colab_b, col_colab_c = st.columns(3)
+    with col_colab_a:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+    with col_colab_b:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+    with col_colab_c:
+        st.image('https://cdn.akamai.steamstatic.com/steam/apps/72850/ss_4e95fbcf72ce2a9f86075738fa9930ef2bed1ac7.1920x1080.jpg?t=1647357402')
+        st.markdown('The Elder Scrolls V Skyrim')
+    
---
+++ recommender/model.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from openai import OpenAI
+import faiss
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+import re
+import streamlit as st
+#setup - embedding
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-large",
+    api_key=st.secrets["API_KEY"]
+    )
+#import vector databases
+VS_content_based = FAISS.load_local(
+    "/content/VS_content_based",
+    embeddings, allow_dangerous_deserialization=True
+    )
+VS_collab_based = FAISS.load_local(
+    "/content/VS_collab_based", embeddings, allow_dangerous_deserialization=True
+)
+
+def recommender(purchased_games, df_feats):
+    #treating game list
+    purchased_games = [re.sub('[^A-Za-z0-9]+', '', i) for i in purchased_games]
+    purchased_games = [i.lower() for i in purchased_games]
+    purchased_games = [i.replace(' ','') for i in purchased_games]
+    
+    #filter DF
+    df_feats = df_feats[df_feats.FK_GAME_NAME.isin(purchased_games)]
+    df_feats = df_feats.drop_duplicates(subset='FK_GAME_NAME')
+    df_feats['explicative_features'] =\
+    df_feats['Tags'].astype(str)\
+    + '|' + df_feats['Categories'].astype(str)\
+    + '|' + df_feats['Supported languages'].astype(str)\
+    + '|' + df_feats['Estimated owners'].astype(str)\
+    + '|' + df_feats['Price'].astype(str)
+    
+    df_feats['explicative_features'] = df_feats['explicative_features'].astype(str).str.replace("'",'No description').str.replace("None",'No description')
+    explicative_features = ' '.join(df_feats['explicative_features'].to_list())
+    content_results = VS_content_based.similarity_search(
+    str(explicative_features),
+    k=30,
+    )
+    content_results_list=[]
+    for i in content_results:
+        content_results_list.append(i.id)
+    collab_results = list(set(content_results_list)-set(purchased_games))
+    return collab_results
